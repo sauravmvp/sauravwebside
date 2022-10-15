3b837f454d18fc33aa1b31ea7b829f6f1a20b17a
sauravmvp <115896458+sauravmvp@users.noreply.github.com>
2022-10-16 00:43:49 +0530

    Add files via upload

---
+++ JavaScript/Contact.js
@@ -0,0 +1,36 @@
+function git() {
+    if (confirm("Do you want to visit saurav mishra gitgub page ")) {
+     location.href="https://github.com/mvpsaurav";
+    }
+ }
+function gmail() {
+    if (confirm("Do you want to compose email to Saurav Mishra")) {
+     location.href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=GTvVlcSDZPJFrglvTfbLBgtrhrpRgXvHrfXGTTtZbBzRFsFZtVdwBPTVGNqlstNhHgpkHqMdfvgSM";
+    }
+ }
+function insta() {
+    if (confirm("Do you want to visit saurav mishra Instagram Page ")) {
+     location.href="https://www.instagram.com/the.sauravmishra/";
+    }
+ }
+
+function whatsapp() {
+    if (confirm("du you want to visit saurav mishra whatsapp page ")) {
+     location.href="/images/whcont.jpg";
+    }
+ }
+
+
+ function btnsubmit(){
+    alert("This form is not working right now you can fill my google form");
+    if(confirm(" If you Want to fill Google from then Then click Yes ")) {
+        location.href="--Link--";
+       }
+ }
+ 
+// function git() {
+//     if (confirm("du you want to go saurav mishra gitgub page ")) {
+//      location.href="https://github.com/mvpsaurav";
+//     }
+//  }
+
